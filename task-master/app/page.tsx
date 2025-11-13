cc21bae02229ca96756d6359cf6a8c0b7df6e89d
RaneshChandraKatta <katta.ranesh.chandra@gmail.com>
2025-11-13 15:37:11 +0530

    commit

--- task-master/app/page.tsx
+++ task-master/app/page.tsx
@@ -3,6 +3,8 @@
 import { ChangeEvent, useState } from "react";
 
 export default function Home() {
+  const [isEditing, setIsEditing] = useState(false);
+  const [isEditingIndex, setIsEditingIndex] = useState(-1);
   const [noteContent, setNoteContent] = useState("");
   const [taskList, setTaskList] = useState([
     "This is task 1",
@@ -14,8 +16,17 @@ export default function Home() {
     setNoteContent(e.target.value);
   };
   const handleSaveNote = () => {
-    const newTaskList = [noteContent, ...taskList];
-    setTaskList(newTaskList);
+    if (isEditing) {
+      const newTaskList = [...taskList];
+      newTaskList[isEditingIndex] = noteContent;
+      setTaskList(newTaskList);
+      setIsEditing(false);
+      setIsEditingIndex(-1);
+      setNoteContent("");
+    } else {
+      const newTaskList = [noteContent, ...taskList];
+      setTaskList(newTaskList);
+    }
   };
 
   const handleDeleteNote = (index: number) => {
@@ -23,19 +34,33 @@ export default function Home() {
     newTaskList.splice(index, 1);
     setTaskList(newTaskList);
   };
+  const handleEditNote = (index: number) => {
+    setNoteContent(taskList[index]);
+    setIsEditing(true);
+    setIsEditingIndex(index);
+  };
 
   return (
     <div className="flex min-h-screen items-center justify-center font-sans ">
       <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16  sm:items-start">
         <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
           <div>
-            Content{" "}
+            Content
             <textarea //
               value={noteContent}
               onChange={handleNoteContentChanged}
             ></textarea>
+            <br />
+            {isEditing && ( //
+              <div>Editing Mode: On</div>
+            )}
           </div>
-          <button onClick={handleSaveNote}>Save</button>
+          <button //
+            className="hover:bg-green-300! p-20"
+            onClick={handleSaveNote}
+          >
+            Save
+          </button>
 
           <hr />
 
@@ -44,8 +69,25 @@ export default function Home() {
             {taskList.map((taskName, index) => {
               return (
                 <li key={taskName}>
-                  {taskName}
-                  <button onClick={() => handleDeleteNote(index)}>X</button>
+                  <span
+                    className={
+                      isEditing && index === isEditingIndex ? "bg-blue-200" : ""
+                    }
+                  >
+                    {taskName}
+                  </span>
+                  <button
+                    className="hover:bg-blue-300! p-20"
+                    onClick={() => handleEditNote(index)}
+                  >
+                    Edit
+                  </button>
+                  <button
+                    className="hover:bg-red-600! p-20"
+                    onClick={() => handleDeleteNote(index)}
+                  >
+                    X
+                  </button>{" "}
                 </li>
               );
             })}
